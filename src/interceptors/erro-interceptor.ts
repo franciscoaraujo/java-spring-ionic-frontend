7834859ce1a1679e3560cc13f506fe35204a8c57
Oliveira Araujo <foaj@gft.com>
2020-01-13 14:35:17 -0300

    Trantando erros 403

--- src/interceptors/erro-interceptor.ts
+++ src/interceptors/erro-interceptor.ts
@@ -1,32 +1,50 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http'
 import { Observable } from 'rxjs/Rx';
+import { StorageService } from '../services/sotrage_service';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+    constructor(public storage: StorageService){
+
+    }
+
     intercept( request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
         return next.handle(request)
-        .catch((error, caugth) => {
-            
-            let errorObj = error;
-            if(errorObj.error){
-                errorObj = errorObj.error;
-            }
+                .catch((error, caugth) => {
+                    let errorObj = error;
+                    
+                    if(errorObj.error){
+                        errorObj = errorObj.error;
+                    }
 
-            if(!error.status){
-                errorObj = JSON.parse(errorObj);
-            }
+                    if(!error.status){
+                        errorObj = JSON.parse(errorObj);
+                    }
 
-            console.log("Erro detectado pelo interceptor!!!");
-            console.log(errorObj);
+                    console.log("Erro detectado pelo interceptor!!!");
+                    console.log(errorObj);
+                    
+                    switch(errorObj.status){
+                        case 403:
+                            this.handle403();
+                            break;
+                    }
 
-            return Observable.throw(errorObj);
-        }) as any;
-        
+
+                    return Observable.throw(errorObj);
+            }) as any;
     }
-   
+    
+
+    handle403(){
+        this.storage.setLocaluser(null);
+    }
+
+
+
 }
 
 export const ErrorInterceptorProvider = {
